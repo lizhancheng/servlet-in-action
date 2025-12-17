3ba9f7a3111ea26424bb38d52b1c2a70fdecafdc
zhancheng.li <zhancheng.li@zhanchenglideMacBook-Pro.local>
2025-12-17 21:50:52 +0800

    wip: learning Servlet

---
+++ src/main/java/com/servlet/Hello.java
@@ -0,0 +1,27 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/")
+public class Hello extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			// 设置响应类型
+			response.setContentType("text/html");
+			// 获取输出流
+			PrintWriter printWriter = response.getWriter();
+			// 写入响应
+			printWriter.write("<h1>Hello World!</h1>");
+			// flush强制输出
+			printWriter.flush();
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+}
