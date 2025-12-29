7c1b007b74767f02bc7f1ec6f26d437fe10d310e
lizhancheng <279623463@qq.com>
2025-12-29 21:38:42 +0800

    wip: learning Servlet session

---
+++ src/main/java/com/servlet/Index.java
@@ -0,0 +1,32 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/")
+public class Index extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String user = (String) request.getSession().getAttribute("username");
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		response.setHeader("X-Powered-By", "JavaEE Servlet");
+		
+		PrintWriter printWriter = response.getWriter();
+		
+		printWriter.write("""
+				<h1>Welcome home, %s!</h1>
+				""".formatted(user));
+		
+		if (user == null) {
+			printWriter.write("<a href=\"/signin\">Sign In</a>");
+		} else {
+			printWriter.write("<a href=\"/signout\">Sign Out</a>");
+		}
+	}
+}
--- src/main/java/com/servlet/SignIn.java
+++ src/main/java/com/servlet/SignIn.java
@@ -1,6 +1,8 @@
 package com.servlet;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,18 +11,42 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/signin")
 public class SignIn extends HttpServlet {
+	private Map<String, String> users = Map.of("admin", "adminpwd", "test", "testpwd", "guest", "");
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 		String ip = request.getRemoteAddr();
 		response.setContentType("text/html");
 		
 		try {
-			
+			response.setContentType("text/html");
 			PrintWriter printWriter = response.getWriter();
-			
-			printWriter.write("<h1>Your Ip Address is: " + ip + "</h1>");
+			String output = """
+						<h1>UserIp: %s</h1>
+						<form action="/signin" method="post">
+							<label id="username">username: <input name="username"></label>
+							<label id="password">password: <input type="password" name="password"></label>
+							<button type="submit">Submit</button>
+							<button type="reset">Reset</button>
+						</form>
+					""".formatted(ip);
+			printWriter.write(output);
 			printWriter.flush();
 		} catch (Exception e) {
 			// TODO: handle exception
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		String correctedPwd = users.get(username.toLowerCase());
+		if (correctedPwd.equals(password)) {
+			// 登录
+			request.getSession().setAttribute("username", username);
+			response.sendRedirect("/");
+		} else {
+			// 不正确则不可登录
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
 		}
 	}
 }
